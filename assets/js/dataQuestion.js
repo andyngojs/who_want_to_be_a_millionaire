d4fb882b88b7703f60b78ba990adcc525d8f50d6
tranmanhduc-mg <shotshop44@gmail.com>
2021-09-01 20:49:46 +0700

    million

---
+++ assets/js/dataQuestion.js
@@ -0,0 +1,92 @@
+const questions = [
+  {
+    question: "Đại dịch Covid-19 bắt đầu từ năm nào?",
+    answers: [
+      { text: "A. Cuối năm 2019", correct: true },
+      { text: "B. Đầu năm 2020", correct: false },
+      { text: "C. Cuối năm 2020", correct: false },
+      { text: "D. Đầu năm 2021", correct: false },
+    ],
+  },
+  {
+    question: "Nghệ sĩ nào tuyên bố khiêu chiến với đại gia Phương Hằng?",
+    answers: [
+      { text: "A. Thu Minh", correct: false },
+      { text: "B. Hồ Ngọc Hà", correct: false },
+      { text: "C. Đàm Vĩnh Hưng", correct: true },
+      { text: "D. Đức Huy", correct: false },
+    ],
+  },
+  {
+    question: "Ai là nhà sáng lập facebook?",
+    answers: [
+      { text: "Bill Gates", correct: false },
+      { text: "Mark Zuckerberg", correct: true },
+      { text: "Jeff Bezos", correct: false },
+      { text: "Lary Page", correct: false },
+    ],
+  },
+  {
+    question: "Vạn lý trường thành nằm ở đâu?",
+    answers: [
+      { text: "A. Nhật Bản", correct: false },
+      { text: "B. Trung Quốc", correct: true },
+      { text: "C. Hàn Quốc", correct: false },
+      { text: "D. Ấn Độ", correct: false },
+    ],
+  },
+  {
+    question: "Us Open là giải đấu tennis được tổ chức ở quốc gia nào?",
+    answers: [
+      { text: "A. Mỹ", correct: true },
+      { text: "B. Australia", correct: false },
+      { text: "C. Pháp", correct: false },
+      { text: "D. Anh", correct: false },
+    ],
+  },
+  {
+    question: "Quân Sĩ chỉ có trong bộ cờ nào?",
+    answers: [
+      { text: "A. Cờ vua", correct: false },
+      { text: "B. Cờ caro", correct: false },
+      { text: "C. Cờ tướng", correct: true },
+      { text: "D. Cờ cá ngựa", correct: false },
+    ],
+  },
+  {
+    question: "Kênh Youtube cá nhân nào đạt 10tr sub nhanh nhất Việt Nam ?",
+    answers: [
+      { text: "A. Cris Devil Gamer", correct: false },
+      { text: "B. NTN Vlogs", correct: true },
+      { text: "C. Thơ Nguyễn", correct: false },
+      { text: "D. Sơn Tùng MTP", correct: false },
+    ],
+  },
+  {
+    question: "Bài hát 'Muộn rồi mà sao còn' là của ai?",
+    answers: [
+      { text: "A. A.C Xuân Tài", correct: false },
+      { text: "B. Sơn Tùng MTP", correct: true },
+      { text: "C. Tùng Sơn", correct: false },
+      { text: "D. Jack", correct: false },
+    ],
+  },
+  {
+    question: "Câu: Có công mài sắt có ngày nên kim thuộc thể loại nào ?",
+    answers: [
+      { text: "A. Ca dao", correct: false },
+      { text: "B. Tục ngữ", correct: true },
+      { text: "C. Thành ngữ", correct: false },
+      { text: "D. Danh ngôn", correct: false },
+    ],
+  },
+  {
+    question: "Đội tuyển liên quân Việt Nam nào đã vô địch mùa giải đấu trường danh vọng mùa xuân 2021?",
+    answers: [
+      { text: "A. Saigon Phantom", correct: false },
+      { text: "B. Team Flash", correct: true },
+      { text: "C. V Gaming", correct: false },
+      { text: "D. Fap Esports", correct: false },
+    ],
+  },
+];
