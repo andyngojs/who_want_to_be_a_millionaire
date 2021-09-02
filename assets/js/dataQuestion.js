1c54e9a1f3c798b330043375d7e290a54c5cdb4b
tranmanhduc-mg <shotshop44@gmail.com>
2021-09-02 22:12:53 +0700

    duc pc

---
+++ assets/js/dataQuestion.js
@@ -0,0 +1,146 @@
+const questions = [
+  {
+    question: "Đại dịch Covid-19 bắt đầu từ năm nào?",
+    answers: [
+      { text: "A. Cuối năm 2019", correct: true },
+      { text: "B. Đầu năm 2020", correct: false },
+      { text: "C. Cuối năm 2020", correct: false },
+      { text: "D. Đầu năm 2021", correct: false },
+    ],
+  },
+  {
+    question: "Nghệ sĩ nào tuyên bố khiêu chiến với đại gia Phương Hằng?",
+    answers: [
+      { text: "A. Thu Minh", correct: false },
+      { text: "B. Hồ Ngọc Hà", correct: false },
+      { text: "C. Đàm Vĩnh Hưng", correct: true },
+      { text: "D. Đức Huy", correct: false },
+    ],
+  },
+  {
+    question: "Ai là nhà sáng lập facebook?",
+    answers: [
+      { text: "Bill Gates", correct: false },
+      { text: "Mark Zuckerberg", correct: true },
+      { text: "Jeff Bezos", correct: false },
+      { text: "Lary Page", correct: false },
+    ],
+  },
+  {
+    question: "Vạn lý trường thành nằm ở đâu?",
+    answers: [
+      { text: "A. Nhật Bản", correct: false },
+      { text: "B. Trung Quốc", correct: true },
+      { text: "C. Hàn Quốc", correct: false },
+      { text: "D. Ấn Độ", correct: false },
+    ],
+  },
+  {
+    question: "Us Open là giải đấu tennis được tổ chức ở quốc gia nào?",
+    answers: [
+      { text: "A. Mỹ", correct: true },
+      { text: "B. Australia", correct: false },
+      { text: "C. Pháp", correct: false },
+      { text: "D. Anh", correct: false },
+    ],
+  },
+  {
+    question: "Quân Sĩ chỉ có trong bộ cờ nào?",
+    answers: [
+      { text: "A. Cờ vua", correct: false },
+      { text: "B. Cờ caro", correct: false },
+      { text: "C. Cờ tướng", correct: true },
+      { text: "D. Cờ cá ngựa", correct: false },
+    ],
+  },
+  {
+    question: "Kênh Youtube cá nhân nào đạt 10tr sub nhanh nhất Việt Nam ?",
+    answers: [
+      { text: "A. Cris Devil Gamer", correct: false },
+      { text: "B. NTN Vlogs", correct: true },
+      { text: "C. Thơ Nguyễn", correct: false },
+      { text: "D. Sơn Tùng MTP", correct: false },
+    ],
+  },
+  {
+    question: "Bài hát 'Muộn rồi mà sao còn' là của ai?",
+    answers: [
+      { text: "A. A.C Xuân Tài", correct: false },
+      { text: "B. Sơn Tùng MTP", correct: true },
+      { text: "C. Tùng Sơn", correct: false },
+      { text: "D. Jack", correct: false },
+    ],
+  },
+  {
+    question: "Câu: Có công mài sắt có ngày nên kim thuộc thể loại nào ?",
+    answers: [
+      { text: "A. Ca dao", correct: false },
+      { text: "B. Tục ngữ", correct: true },
+      { text: "C. Thành ngữ", correct: false },
+      { text: "D. Danh ngôn", correct: false },
+    ],
+  },
+  {
+    question: "Đội tuyển liên quân Việt Nam nào đã vô địch mùa giải đấu trường danh vọng mùa xuân 2021?",
+    answers: [
+      { text: "A. Saigon Phantom", correct: false },
+      { text: "B. Team Flash", correct: true },
+      { text: "C. V Gaming", correct: false },
+      { text: "D. Fap Esports", correct: false },
+    ],
+  },
+  {
+    question: "Đội bóng quốc gia nào vô địch Euro 2020?",
+    answers: [
+      { text: "A. Bỉ", correct: false },
+      { text: "B. Anh", correct: false },
+      { text: "C. Italia", correct: true },
+      { text: "D. Tây Ban Nha", correct: false },
+    ],
+  },
+  {
+    question: "Bức tranh Em Thúy nổi tiếng của họa sĩ nào?",
+    answers: [
+      { text: "A. Tô Ngọc Vân", correct: false },
+      { text: "B. Bùi Xuân Phái", correct: true },
+      { text: "C. Nguyễn Gia Trí", correct: false },
+      { text: "D. Trần Văn Cẩn", correct: true },
+    ],
+  },
+  {
+    question: "Ai đã lập hội Việt Nam Quang Phục Hội?",
+    answers: [
+      { text: "A. Phan Châu Trinh", correct: false },
+      { text: "B. Phan Bội Châu", correct: true },
+      { text: "C. Đề Thám", correct: false },
+      { text: "D. Đề Nắm", correct: false },
+    ],
+  },
+  {
+    question: "World Cup 2018 được tổ chức ở quốc gia nào?",
+    answers: [
+      { text: "A. Nga", correct: true },
+      { text: "B. Qatar", correct: false },
+      { text: "C. Pháp", correct: false },
+      { text: "D. Đức", correct: false },
+    ],
+  },
+  {
+    question: "Trên la bàn, kim chỉ hướng Đông hợp với kim chỉ hướng Nam 1 góc bao nhiêu độ ?",
+    answers: [
+      { text: "A. 45 độ", correct: false },
+      { text: "B. 90 độ", correct: true },
+      { text: "C. 120 độ", correct: false },
+      { text: "D. 180 độ", correct: false },
+    ],
+  },
+  {
+    question: "Rét nàng Bân ở nước ta thường xảy ra vào tháng mấy âm lịch?",
+    answers: [
+      { text: "A. Tháng 1", correct: false },
+      { text: "B. Tháng 2", correct: false },
+      { text: "C. Tháng 3", correct: true },
+      { text: "D. Tháng 4", correct: false },
+    ],
+  },
+];
