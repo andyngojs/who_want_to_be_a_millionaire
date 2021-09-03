49f0387c0d0d40560c194c239b0f50fed5e61634
andyngojs <dongngo.2000@gmail.com>
2021-09-03 17:08:44 +0700

    fix some issue and add sound

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,30 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+let bgMusic = new Audio("./assets/music/music-start.mp3");
+
 let playBtn = $("#play-btn");
+let startBtn = $(".btn-start");
+let welcomeSection = $(".welcome__start");
+let playSection = $(".play-container");
+let modal = $(".modal");
 
-const app = {
-  handleEvent: () => {
-    playBtn.addEventListener("click", () => {
-      $(".modal").classList.add("show");
-    });
-  },
-  start: function () {
-    this.handleEvent();
-  },
-};
+function startGame() {
+  userHandler();
+}
 
-app.start();
+function userHandler() {
+  playBtn.addEventListener("click", () => {
+    bgMusic.play();
+    modal.classList.add("show");
+  });
+
+  startBtn.addEventListener("click", () => {
+    welcomeSection.classList.add("hide");
+    playSection.classList.remove("hide");
+
+    modal.classList.remove("show");
+  });
+}
+
+startGame();
