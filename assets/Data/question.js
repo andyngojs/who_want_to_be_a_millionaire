50028ba2d89ea4d794092fb34d47edaee1cc34db
andyngojs <dongngo.2000@gmail.com>
2021-09-08 00:28:23 +0700

    format code and fix some questions

--- assets/Data/question.js
+++ assets/Data/question.js
@@ -2,162 +2,118 @@ const questions = [
   {
     question: "Đại dịch Covid-19 bắt đầu từ năm nào?",
     answers: [
-      'A.Đầu năm 2019',
-      'B.Cuối năm 2019',
-      'C.Đầu năm 2020',
-      'D.Cuối năm 2020'
+      "A.Đầu năm 2019",
+      "B.Cuối năm 2019",
+      "C.Đầu năm 2020",
+      "D.Cuối năm 2020",
     ],
-    correct:1
+    correct: 1,
   },
   {
     question: "Nghệ sĩ nào tuyên bố khiêu chiến với đại gia Phương Hằng?",
-    answers: [
-       "A. Thu Minh", 
-       "B. Hồ Ngọc Hà", 
-       "C. Đàm Vĩnh Hưng", 
-       "D. Đức Huy", 
-    ],
-    correct:2
+    answers: ["A. Thu Minh", "B. Hồ Ngọc Hà", "C. Đàm Vĩnh Hưng", "D. Đức Huy"],
+    correct: 2,
   },
   {
     question: "Ai là nhà sáng lập facebook?",
     answers: [
-       "A. Bill Gates", 
-       "B. Mark Zuckerberg", 
-       "C. Jeff Bezos", 
-       "D. Lary Page", 
+      "A. Bill Gates",
+      "B. Mark Zuckerberg",
+      "C. Jeff Bezos",
+      "D. Lary Page",
     ],
-    correct:1
+    correct: 1,
   },
   {
-    question: "Vạn lý trường thành nằm ở đâu?",
+    question: "Ai là người đẹp trai nhất ?",
     answers: [
-       "A. Nhật Bản", 
-       "B. Trung Quốc", 
-       "C. Hàn Quốc", 
-       "D. Ấn Độ", 
+      "A. Trần Đức",
+      "B. AndyNgoJs",
+      "C. Nguyễn Kỳ Anh",
+      "D. A.D Đông Ngô",
     ],
-    correct:1
+    correct: 3,
   },
   {
     question: "Us Open là giải đấu tennis được tổ chức ở quốc gia nào?",
-    answers: [
-       "A. Mỹ", 
-       "B. Australia", 
-       "C. Pháp", 
-       "D. Anh", 
-    ],
-    correct:1
+    answers: ["A. Mỹ", "B. Australia", "C. Pháp", "D. Anh"],
+    correct: 1,
   },
   {
     question: "Quân Sĩ chỉ có trong bộ cờ nào?",
-    answers: [
-       "A. Cờ vua", 
-       "B. Cờ caro", 
-       "C. Cờ tướng", 
-       "D. Cờ cá ngựa", 
-    ],
-    correct:2
+    answers: ["A. Cờ vua", "B. Cờ caro", "C. Cờ tướng", "D. Cờ cá ngựa"],
+    correct: 2,
   },
   {
     question: "Kênh Youtube cá nhân nào đạt 10tr sub nhanh nhất Việt Nam ?",
     answers: [
-       "A. Cris Devil Gamer", 
-       "B. NTN Vlogs", 
-       "C. Thơ Nguyễn", 
-       "D. Sơn Tùng MTP", 
+      "A. Cris Devil Gamer",
+      "B. NTN Vlogs",
+      "C. Thơ Nguyễn",
+      "D. Sơn Tùng MTP",
     ],
-    correct:1
+    correct: 1,
   },
   {
-    question: "Bài hát 'Muộn rồi mà sao còn' là của ai?",
-    answers: [
-       "A. A.C Xuân Tài", 
-       "B. Sơn Tùng MTP", 
-       "C. Tùng Sơn", 
-       "D. Jack", 
-    ],
-    correct:1
+    question: "Bài hát 'Độ Tộc 2' là của ai?",
+    answers: ["A. Phúc Du", "B. Pháo", "C. Độ Mixi", "D. Masew"],
+    correct: 2,
   },
   {
     question: "Câu: Có công mài sắt có ngày nên kim thuộc thể loại nào ?",
-    answers: [
-       "A. Ca dao", 
-       "B. Tục ngữ", 
-       "C. Thành ngữ", 
-       "D. Danh ngôn", 
-    ],
-    correct:1
+    answers: ["A. Ca dao", "B. Tục ngữ", "C. Thành ngữ", "D. Danh ngôn"],
+    correct: 1,
   },
   {
-    question: "Đội tuyển liên quân Việt Nam nào đã vô địch mùa giải đấu trường danh vọng mùa xuân 2021?",
+    question:
+      "Đội tuyển liên quân Việt Nam nào đã vô địch mùa giải đấu trường danh vọng mùa xuân 2021?",
     answers: [
-       "A. Saigon Phantom", 
-       "B. Team Flash", 
-       "C. V Gaming", 
-       "D. Fap Esports", 
+      "A. Saigon Phantom",
+      "B. Team Flash",
+      "C. V Gaming",
+      "D. Fap Esports",
     ],
-    correct:1
+    correct: 1,
   },
   {
     question: "Đội bóng quốc gia nào vô địch Euro 2020?",
-    answers: [
-       "A. Bỉ", 
-       "B. Anh", 
-       "C. Italia", 
-       "D. Tây Ban Nha", 
-    ],
-    correct:2
+    answers: ["A. Bỉ", "B. Anh", "C. Italia", "D. Tây Ban Nha"],
+    correct: 2,
   },
   {
     question: "Bức tranh Em Thúy nổi tiếng của họa sĩ nào?",
     answers: [
-       "A. Tô Ngọc Vân", 
-       "B. Bùi Xuân Phái", 
-       "C. Nguyễn Gia Trí", 
-       "D. Trần Văn Cẩn", 
+      "A. Tô Ngọc Vân",
+      "B. Bùi Xuân Phái",
+      "C. Nguyễn Gia Trí",
+      "D. Trần Văn Cẩn",
     ],
-    correct:3
+    correct: 3,
   },
   {
     question: "Ai đã lập hội Việt Nam Quang Phục Hội?",
     answers: [
-       "A. Phan Châu Trinh", 
-       "B. Phan Bội Châu", 
-       "C. Đề Thám", 
-       "D. Đề Nắm", 
+      "A. Phan Châu Trinh",
+      "B. Phan Bội Châu",
+      "C. Đề Thám",
+      "D. Đề Nắm",
     ],
-    correct:2
+    correct: 2,
   },
   {
     question: "World Cup 2018 được tổ chức ở quốc gia nào?",
-    answers: [
-       "A. Nga", 
-       "B. Qatar", 
-       "C. Pháp", 
-       "D. Đức", 
-    ],
-    correct:0
+    answers: ["A. Nga", "B. Qatar", "C. Pháp", "D. Đức"],
+    correct: 0,
   },
   {
-    question: "Trên la bàn, kim chỉ hướng Đông hợp với kim chỉ hướng Nam 1 góc bao nhiêu độ ?",
-    answers: [
-       "A. 45 độ", 
-       "B. 90 độ", 
-       "C. 120 độ", 
-       "D. 180 độ", 
-    ],
-    correct:2
+    question:
+      "Trên la bàn, kim chỉ hướng Đông hợp với kim chỉ hướng Nam 1 góc bao nhiêu độ ?",
+    answers: ["A. 45 độ", "B. 90 độ", "C. 120 độ", "D. 180 độ"],
+    correct: 2,
   },
   {
     question: "Rét nàng Bân ở nước ta thường xảy ra vào tháng mấy âm lịch?",
-    answers: [
-       "A. Tháng 1", 
-       "B. Tháng 2", 
-       "C. Tháng 3", 
-       "D. Tháng 4", 
-    ],
-    correct:2
+    answers: ["A. Tháng 1", "B. Tháng 2", "C. Tháng 3", "D. Tháng 4"],
+    correct: 2,
   },
 ];
-
